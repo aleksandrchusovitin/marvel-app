cb0ed7b87ebb26dbf08da511093a3c9b3f9583a9
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-11-01 16:07:10 +0700

    add suspense

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,14 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import AppHeader from '../appHeader/AppHeader';
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
+import Spinner from '../spinner/Spinner';
+
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
   return (
@@ -9,20 +16,22 @@ const App = () => {
       <div className='app'>
         <AppHeader />
         <main>
-          <Switch>
-            <Route exact path='/'>
-              <MainPage />
-            </Route>
-            <Route exact path='/comics'>
-              <ComicsPage />
-            </Route>
-            <Route exact path='/comics/:comicId'>
-              <SingleComicPage />
-            </Route>
-            <Route path='*'>
-              <Page404 />
-            </Route>
-          </Switch>
+          <Suspense fallback={<Spinner />}>
+            <Switch>
+              <Route exact path='/'>
+                <MainPage />
+              </Route>
+              <Route exact path='/comics'>
+                <ComicsPage />
+              </Route>
+              <Route exact path='/comics/:comicId'>
+                <SingleComicPage />
+              </Route>
+              <Route path='*'>
+                <Page404 />
+              </Route>
+            </Switch>
+          </Suspense>
         </main>
       </div>
     </Router>
