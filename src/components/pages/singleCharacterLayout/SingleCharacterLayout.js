6f79f32912b5f738cc8876e152a0c566a0103556
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-11-10 21:17:48 +0700

    add react helmet to single pages

--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import './singleCharacterLayout.scss';
 
@@ -6,16 +7,25 @@ const SingleCharacterLayout = ({ data }) => {
   const { name, description, thumbnail } = data;
 
   return (
-    <div className='single-character'>
-      <img src={thumbnail} alt={name} className='single-character__char-img' />
-      <div className='single-character__info'>
-        <h2 className='single-character__name'>{name}</h2>
-        <p className='single-character__descr'>{description}</p>
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content={`${name} character page`}
+        />
+        <title>{name}</title>
+      </Helmet>
+      <div className='single-character'>
+        <img src={thumbnail} alt={name} className='single-character__char-img' />
+        <div className='single-character__info'>
+          <h2 className='single-character__name'>{name}</h2>
+          <p className='single-character__descr'>{description}</p>
+        </div>
+        <Link to='/' className='single-character__back'>
+          Back to homepage
+        </Link>
       </div>
-      <Link to='/' className='single-character__back'>
-        Back to homepage
-      </Link>
-    </div>
+    </>
   );
 };
 
